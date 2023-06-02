13d12609251026469673fd6e10ed4b7fbd75a72a
Aly Ahmed <113694375+AlyAhmedRbk@users.noreply.github.com>
2023-06-02 16:09:23 +0500

    Add files via upload
    Assignment Number 3

--- App.js
+++ App.js
@@ -1,55 +1,83 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import *  as React from 'react';
+import { StyleSheet, Button, TextInput, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login } from "./components/Login";
+import { Profile } from './components/Profile';
+import { Signup } from './components/Signup';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const App = () => {
-  const boardSize = 8;
-  const board = [];
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
-  for (let i = 0; i < boardSize; i++) {
-    const row = [];
-    for (let j = 0; j < boardSize; j++) {
-      const isBlack = (i + j) % 2 === 1;
-      const squareStyle = [styles.square, isBlack ? styles.blackSquare : styles.whiteSquare];
-      row.push(<View key={`${i}${j}`} style={squareStyle} />);
-    }
-    board.push(<View key={i} style={styles.boardRow}>{row}</View>);
-  }
 
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Chess Layout</Text>
-      {board}
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator
+
+          screenOptions={{
+            headerRight: (props) => { return (<Button title='Right' color={'green'} />) },
+            headerStyle: {
+              backgroundColor: 'orange'
+            },
+            headerTintColor: "white",
+            headerTitleStyle: {
+              fontSize: 25,
+            }
+          }}
+        >
+
+          <Stack.Screen name="Login" component={Login}
+
+          // options={{
+          //   // headerTitle :(props) => {return(<Button title='left'/>)} ,
+          //   // headerRight : (props) =>{return (<Button title='Right'/>)},
+          //   title : "User Login",
+          //   headerStyle : {
+          //     backgroundColor : 'orange'
+          //   },
+          //   headerTintColor: "blue",
+          //   headerTitleStyle : {
+          //     fontSize : 45,
+          //   }
+          // }}
+          />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="Signup" component={Signup} />
+
+        </Stack.Navigator>
+      </NavigationContainer>
+
+
+      <View>
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="User Login" component={userLogin} />
+            <Tab.Screen name="Admin Login" component={adminLogin} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  container: {
-    // backgroundColor: 'brown',
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boardRow: {
-    flexDirection: 'row',
-  },
-  square: {
-    borderWidth : 2,
-    width: 50,
-    height: 50,
-  },
-  blackSquare: {
-    backgroundColor: 'black',
-  },
-  whiteSquare: {
-    backgroundColor: 'white',
-  },
-  text: {
-    padding: 10,
-    fontSize : 40,
-    fontWeight : 'bold',
-    margin : 10,
-  }
-});
+const userLogin = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>User Login</Text>
+    </View>
+  )
+}
+const adminLogin = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Admin Login</Text>
+    </View>
+  )
+}
 
-export default App;
+
+
